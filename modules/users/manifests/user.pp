42bdb7f78063e6133f3940055ccf477f9bb9b76d
jcarley <jcarley@gettyimages.com>
2013-08-18 10:11:48 -0500

    Adds password expiration

--- modules/users/manifests/user.pp
+++ modules/users/manifests/user.pp
@@ -3,10 +3,19 @@ define user::user () {
   $pwd = '$6$0VaH1y/8$v5TvLL8CITIx/6EB1KVa0JSrYoKPC6zdNueVEMb.Ak7ekTXz2WFOnTlf3KVDUZzEtdmYkc09bzXekJ/sDUya8/'
 
   user { "$name":
-    ensure     => present,
-    shell      => '/bin/bash',
-    home       => "/home/$name",
-    password   => $pwd,
-    managehome => true,
+    ensure           => present,
+    shell            => '/bin/bash',
+    home             => "/home/$name",
+    password         => $pwd,
+    password_max_age => 0,
+    password_min_age => 0,
+    managehome       => true,
+  } ->
+
+  exec { "passwd -e $name":
+    user  => 'root',
+    group => 'root',
   }
+
+
 }
